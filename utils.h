06254e1b53f7484d4ee0a90360eab0b8537d1ebb
koreanwglasses <koreanwglasses@gmail.com>
2020-04-29 20:57:33 -0400

    docs

--- utils.h
+++ utils.h
@@ -8,6 +8,10 @@
 template <unsigned int blockSize>
 __global__ void fmin_kernel(float *g_idata, float *g_odata, unsigned int n);
 
+// Computes the min of values in d_idata and stores the result (per block) in
+// d_odata
+// d_idata should have `n` floats
+// d_odata should have enough memory for `blocks` floats
 void launch_fmin_kernel (
   float * d_idata, 
   float * d_odata, 
